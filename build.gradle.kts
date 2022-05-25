44c61e0cc8b9a86b462f20006153d548783e47ce
Trond Anders Sorvoja <sorvoja@gmail.com>
2022-05-25 09:51:59 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 
 sonarqube {
     properties {
-        property("sonar.projectKey", "navikt_im-varsel")
+        property("sonar.projectKey", "navikt_helsearbeidsgiver-pdl-client")
         property("sonar.organization", "navikt")
         property("sonar.host.url", "https://sonarcloud.io")
         property("sonar.login", System.getenv("SONAR_TOKEN"))
