c24862ca3377181e9c0f54eb44b24cd4afa784f9
Mikael Bjerga <mikael.bjerga@nav.no>
2023-06-22 15:07:21 +0200

    Oppdater URL til behandligskatalog

--- src/main/kotlin/no/nav/helsearbeidsgiver/pdl/Behandlingsgrunnlag.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/pdl/Behandlingsgrunnlag.kt
@@ -2,11 +2,11 @@ package no.nav.helsearbeidsgiver.pdl
 
 /**
  * Oversikt over tilgjengelige behandlingsgrunnlag og mulige utvidelser finnes i
- * [behandlingskatalogen for sykepenger](https://behandlingskatalog.nais.adeo.no/process/purpose/SYKEPENGER).
+ * [behandlingskatalogen for sykepenger](https://behandlingskatalog.intern.nav.no/process/purpose/SYKEPENGER).
  */
 enum class Behandlingsgrunnlag(
     internal val behandlingsnummer: String
 ) {
-    /** Dokumentert i [behandlingskatalog](https://behandlingskatalog.nais.adeo.no/process/purpose/SYKEPENGER/e1712d5c-f3e1-48c7-a830-a2da90482253). */
+    /** Dokumentert i [behandlingskatalog](https://behandlingskatalog.intern.nav.no/process/purpose/SYKEPENGER/e1712d5c-f3e1-48c7-a830-a2da90482253). */
     INNTEKTSMELDING("B190")
 }
