cf5dd2ddef9f6c5acce2b7361462cc7974fe592a
Mikael Bjerga <mikael.bjerga@nav.no>
2023-09-29 09:45:36 +0200

    Bump avhengigheter

--- src/main/kotlin/no/nav/helsearbeidsgiver/pdl/Utils.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/pdl/Utils.kt
@@ -5,7 +5,7 @@ import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.apache5.Apache5
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
-import no.nav.helsearbeidsgiver.utils.json.jsonIgnoreUnknown
+import no.nav.helsearbeidsgiver.utils.json.jsonConfig
 
 internal fun createHttpClient(): HttpClient =
     HttpClient(Apache5) { configure() }
@@ -14,9 +14,6 @@ internal fun HttpClientConfig<*>.configure() {
     expectSuccess = true
 
     install(ContentNegotiation) {
-        json(jsonIgnoreUnknown)
+        json(jsonConfig)
     }
 }
-
-internal fun String.readResource(): String =
-    ClassLoader.getSystemResource(this).readText()
