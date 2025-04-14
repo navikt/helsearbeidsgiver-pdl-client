bea606d9715012d7c7601b2dcb3328287d052f96
Jesper Hustad <jesper.forrest.hustad@nav.no>
2025-04-14 10:43:23 +0200

    Legg til sykmelding grunnlag

--- src/main/kotlin/no/nav/helsearbeidsgiver/pdl/Behandlingsgrunnlag.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/pdl/Behandlingsgrunnlag.kt
@@ -10,4 +10,7 @@ enum class Behandlingsgrunnlag(
     /** Dokumentert i [behandlingskatalog](https://behandlingskatalog.intern.nav.no/process/purpose/SYKEPENGER/e1712d5c-f3e1-48c7-a830-a2da90482253). */
     INNTEKTSMELDING("B190"),
     FRITAKAGP("B329"),
+
+    /** Dokumentert i [behandlingskatalog](https://behandlingskatalog.intern.nav.no/process/purpose/SYKEPENGER/250c33c1-183c-49cc-9f3b-fdfe9f05f486). */
+    SYKMELDING("B229"),
 }
